3a0c4e5937266dbac768a75063bccdb71711bda4
jashby1993 <jashby93school@gmail.com>
2024-09-30 21:40:00 -0600

    Days to Christmas

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+DateTime today = DateTime.Today;
+DateTime CHRISTMAS = new DateTime(today.Year,12,25);
+TimeSpan difference = CHRISTMAS - today;
+int days_to_christmas = (int)difference.TotalDays;
+
+Console.WriteLine($"Only {days_to_christmas} days until Christmas!");
