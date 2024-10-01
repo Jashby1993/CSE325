9975c5288a64879e85044b3361c0fbdeed97de2a
jashby1993 <jashby93school@gmail.com>
2024-09-30 22:39:51 -0600

    OH my gosh I'm so behind.

---
+++ dotNetDependencies/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+
+Console.WriteLine("Hello, World!");
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.Now)
+}
